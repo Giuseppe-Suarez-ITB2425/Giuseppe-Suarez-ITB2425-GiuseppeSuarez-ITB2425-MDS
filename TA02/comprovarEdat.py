0063eb461d23dc630c3b642902457020cba2d728
Giuseppe <stalyn.suarez.7e8@itb.cat>
2024-09-27 08:22:33 +0200

    Edad

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,20 @@
+"""
+Javier Amaya Boira
+
+12/09/2023
+
+ASIXc M03 UF1 A1
+
+Descripció: Exemple dels apunts
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat = int(input("Quina edat tens? "))
+
+if edat >= 18:
+    print("Ets major d'edat")
+else:
+    print("No ets major d'edat")
+
+print("Programa Finalitzat")
