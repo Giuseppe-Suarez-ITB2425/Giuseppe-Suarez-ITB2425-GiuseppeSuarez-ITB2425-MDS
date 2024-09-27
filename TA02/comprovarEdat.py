83a5be6fa16af2547523fe255cf6b891afc33e0c
Giuseppe <stalyn.suarez.7e8@itb.cat>
2024-09-27 08:47:14 +0200

    Edad Creamos una nueva variable llamado anño_actual para calcular el año de nacimiento

--- TA02/comprovarEdat.py
+++ TA02/comprovarEdat.py
@@ -11,10 +11,13 @@ Descripció: Exemple dels apunts
 # Programa que demana l'edat i diu si ets major d'edat.
 
 edat = int(input("Quina edat tens? "))
-
+año_actual = int(input("En quin any estem?"))
 if edat >= 18:
     print("Ets major d'edat")
+elif 13<= edat <18:
+    print("Ets un adolescent")
 else:
     print("No ets major d'edat")
-
+año_nacimiento = año_actual - edat
+print(f"Vas neixer al any: {año_nacimiento} ")
 print("Programa Finalitzat")
